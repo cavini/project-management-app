14be4dd043d602110bfd5da224d32847b2cdcf5f
Arthur Cavini <arthurcavini@Arthurs-MacBook-Pro.local>
2022-09-30 15:53:57 -0300

    chore: project and client queries setup

---
+++ server/models/Project.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const ProjectSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ["Not Started", "In Progress", "Completed"],
+  },
+  // The way we reference other models via ID without using the automatically generated ObjectID is through ref
+  clientId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Client'
+  },
+});
+
+module.exports = mongoose.model("Project", ProjectSchema);
