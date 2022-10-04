c0571adc141934f5f89d796895a8a080e4094ca7
Arthur Cavini <arthurcavini@Arthurs-MacBook-Pro.local>
2022-10-03 23:00:29 -0300

    chore: fixed cors issues on backend

---
+++ client/src/mutations/projectMutations.js
@@ -0,0 +1,17 @@
+import { gql } from '@apollo/client'
+
+const ADD_PROJECT = gql`
+  mutation AddProject(
+    $name: String!
+    $description: String!
+    $status: ProjectStatus!
+    $clientId: ID!
+  ) {
+    addProject(
+      name: $name
+      description: $description
+      status: $status
+      clientId: $clientId
+    )
+  }
+`
